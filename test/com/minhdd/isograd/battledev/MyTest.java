26729fa4188751128cf7a14284e88788bedbd192
Minh Lenovo <md.insa@yahoo.com>
2016-04-01 23:26:29 +0200

    test update

--- test/com/minhdd/isograd/battledev/MyTest.java
+++ test/com/minhdd/isograd/battledev/MyTest.java
@@ -1,13 +1,10 @@
 package com.minhdd.isograd.battledev;
 
 import com.minhdd.isograd.TestConfig;
-import com.minhdd.isograd.Utils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,14 +12,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class MyTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test1() throws FileNotFoundException {
+        test("data/input1.txt", "data/output1.txt");
+    }
+
+    @Test
+    public void test2() throws FileNotFoundException {
+        test("data/input2.txt", "data/output2.txt");
+    }
+
+    private void test(String input, String output) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(input));
     }
 }
